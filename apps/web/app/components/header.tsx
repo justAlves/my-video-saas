606ddac4aec0d6314d64064519426481ba1675ca
Guilherme Alves Profirio <iamalves.jpg@gmail.com>
2025-07-14 23:16:48 -0300

    feat: improve header

--- apps/web/app/components/header.tsx
+++ apps/web/app/components/header.tsx
@@ -1,10 +1,16 @@
 import useGetUser from '@/hooks/use-getuser';
 import { VideoIcon } from 'lucide-react'
-import React, { useEffect } from 'react'
 
 export default function Header() {
 
-  const { data } = useGetUser();
+  const { data: user } = useGetUser();
+
+  const getInitials = (name: string) => {
+    if (!name) return '';
+    const initials = name.split(' ')
+    const initialsString = `${initials[0].charAt(0)}${initials[1].charAt(0) || ''}`
+    return initialsString.toUpperCase();
+  }
 
   return (
     <header
@@ -19,6 +25,14 @@ export default function Header() {
           Vixy
         </h1>
       </div>
+
+      {user && user.image ? (
+        <img src={user.image} alt="User Avatar" className="w-8 h-8 rounded-full" />
+      ) : (
+        <div className="w-8 h-8 rounded-full bg-purple-700 flex justify-center items-center text-sm font-bold">
+          {getInitials(user?.name)}
+        </div>
+      )}
       
     </header>
   )
