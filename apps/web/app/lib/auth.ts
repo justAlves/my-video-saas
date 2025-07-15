065b58029f49beff94ae226ba22220edfc8d0d4d
Guilherme Alves Profirio <iamalves.jpg@gmail.com>
2025-07-14 21:48:08 -0300

    feat: add new back url

--- apps/web/app/lib/auth.ts
+++ apps/web/app/lib/auth.ts
@@ -2,7 +2,7 @@ import { createAuthClient } from "better-auth/react"
 import { adminClient, organizationClient } from "better-auth/client/plugins"
 
 export const auth = createAuthClient({
-  baseURL: "http://localhost:3000",
+  baseURL: import.meta.env.VITE_BACKEND_URL!,
   plugins: [
     adminClient(),
     organizationClient()
